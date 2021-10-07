498c5e756d2c43070adf57c9aebc439eace8065d
Kristian Fulkerson <kristianf451@gmail.com>
2021-10-07 14:19:08 -0700

    added removeItem function

--- src/App.js
+++ src/App.js
@@ -18,11 +18,14 @@ function App() {
   const addItem = (item) => {
     setCart([...cart, item]);
   };
+  const removeItem = (id) => {
+    setCart(cart.filter((item) => item.id !== id));
+  };
 
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <CartContext.Provider value={cart}>
+        <CartContext.Provider value={{ cart, removeItem }}>
           <Navigation />
 
           {/* Routes */}
